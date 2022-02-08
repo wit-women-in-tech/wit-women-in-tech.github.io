e266dde26ea2d6254ffe37ea25d0cc76f04500ff
anshika-18 <74226523+anshika-18@users.noreply.github.com>
2022-02-08 23:02:52 +0530

    use scss

--- src/Pages/Dashboard/Mentee/Dashboard.jsx
+++ src/Pages/Dashboard/Mentee/Dashboard.jsx
@@ -1,5 +1,9 @@
 import React from "react";
-
+import "./Dashboard.scss";
 export default function Dashboard() {
-  return <div>Dashboard Mentee</div>;
+  return (
+    <div className="dashboard">
+      <h1>Dashboard</h1>
+    </div>
+  );
 }
